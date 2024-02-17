c5da8fecf24b36c9e9dcd8120840c61cc410b6de
Ash <ashloraptor@gmail.com>
2024-02-16 20:58:51 -0500

    I haven't been able to make any progress on this since Wednesday, despite dedicating hours each day.

--- db/index.js
+++ db/index.js
@@ -17,11 +17,18 @@ class DB {
   }
   // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() 
-  {
-    return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-    );
-  }
+   {
+  //   return this.connection.promise().query(
+  //     "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+  //   );
+  // }
+
+  connection.query( 'SELECT * FROM employee', ( err, rows ) => {
+    console.log(rows);
+    // do something with the results here
+} );
+};
+  
 
 
 
